724810d69892e0774c35c91a0c173499bbd6dd12
yuiosa <yuiosa@C010423044.zipangu.cac.co.jp>
2026-04-20 16:45:42 +0900

    lesson03challenge06終了

--- Java100_questions_cac2nd/src/lesson03/challenge02/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge02/Explorer.java
@@ -57,17 +57,19 @@ public class Explorer {
 		System.out.println("隊長：");
 		System.out.println("サバ缶を50個以上袋に詰めてください。\n");
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int targetCans = 50;
 		int inputCans = 0;
 
-
 		for (int i = 0; i < 3; i++) {
-			System.out.println("\n隊長":);
-			
-			
-		}
+			System.out.println("\n隊長:");
+			System.out.println("あと" + (3 - i) + "回！");
+			System.out.println("\n袋に何個入れますか？＞");
 
+			String str = reader.readLine();
+			inputCans = Integer.parseInt(str);
+
+		}
 
 		if (inputCans >= targetCans) {
 			System.out.println("\n隊長：");
