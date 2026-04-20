06ffd0c0bb1c84a3eb5aebdfd89de1bdc6b9ce2f
yuiosa <yuiosa@C010423044.zipangu.cac.co.jp>
2026-04-20 23:59:48 +0900

    lesson04challenge02終了

--- Java100_questions_cac2nd/src/lesson04/challenge02/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge02/WarehouseManager.java
@@ -95,22 +95,17 @@ public class WarehouseManager {
 
 		System.out.print("データ型を選んでください（1...文字、2...文字列、3...数値）＞");
 
-
-		//ここに入力処理を記述する。
-
+		int date = Integer.parseInt(br.readLine());
 
 		System.out.print("\n要素数を選んでください（1...1個、2...2個、3...3個）＞");
 
-
-		//ここに入力処理を記述する。
-
+		int num = Integer.parseInt(br.readLine());
 
 		boolean errFlag = false;
 
-
-		//ここに入力値の範囲チェック処理を記述する。
-
-
+		if ((date < 1 || date > 3) || (num < 1 || num > 3)) {
+			errFlag = true;
+		}
 		if (!errFlag) {
 			System.out.println("\nZ先輩：");
 			System.out.println("中に入れる値はおまかせという事でよろしいですね。");
@@ -126,10 +121,52 @@ public class WarehouseManager {
 			String[] strArray = null;
 			int[] intArray = null;
 
+			if (date == 1) {
+				if (num == 1) {
+					charArray = new char[1];
+					charArray[0] = 'a';
+				} else if (num == 2) {
+					charArray = new char[2];
+					charArray[0] = 'a';
+					charArray[1] = 'b';
+				} else {
+					charArray = new char[3];
+					charArray[0] = 'a';
+					charArray[1] = 'b';
+					charArray[2] = 'c';
 
-			//ここに入力値による分岐および配列要素数の確定、
-			//値の代入処理を記述する。
+				}
+			} else if (date == 2) {
+				if (num == 1) {
+					strArray = new String[1];
+					strArray[0] = "abc";
+				} else if (num == 2) {
+					strArray = new String[2];
+					strArray[0] = "abc";
+					strArray[1] = "def";
+				} else {
+					strArray = new String[3];
+					strArray[0] = "abc";
+					strArray[1] = "def";
+					strArray[2] = "ghi";
 
+				}
+
+			} else {
+				if (num == 1) {
+					intArray = new int[1];
+					intArray[0] = 1;
+				} else if (num == 2) {
+					intArray = new int[2];
+					intArray[0] = 1;
+					intArray[1] = 2;
+				} else {
+					intArray = new int[3];
+					intArray[0] = 1;
+					intArray[1] = 2;
+					intArray[2] = 3;
+				}
+			}
 
 			System.out.println("Yさん：");
 			System.out.println("...出来ました。\n");
@@ -139,15 +176,20 @@ public class WarehouseManager {
 
 			System.out.println("Yさん：");
 
-
-			//ここに入力値による分岐および配列要素の表示処理を記述する。
-
-
+			if (date == 1) {
+				System.out.print(charArray[charArray.length - 1]);
+			} else if (date == 2) {
+				System.out.print(strArray[strArray.length - 1]);
+			} else {
+				System.out.print(intArray[intArray.length - 1]);
+			}
 			System.out.println("です。\n");
 
 			System.out.println("Z先輩：");
 			System.out.println("はい、ありがとう。ちゃんと出来てますね。\n");
-		} else {
+		} else
+
+		{
 			System.out.println("\nZ先輩：");
 			System.out.println("そのような選択肢はありません。");
 		}
