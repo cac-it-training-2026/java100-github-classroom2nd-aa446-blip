5761120d44ff40de4f4bc0a32f4b09a70f0ca6d4
yuiosa <yuiosa@C010423044.zipangu.cac.co.jp>
2026-04-28 17:26:07 +0900

    lesson07challenge13終了

--- Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
+++ Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
@@ -23,37 +23,109 @@
 
 package lesson07.challenge11;
 
-
 //ここにSamuraiクラスを記述
+abstract class Samurai {
+	abstract void work();
 
+	protected String name;
+
+	void fight() {
+		System.out.println("戦うよ～。");
+	}
+
+}
 
 //ここにRetainerクラスを記述
+class Retainer extends Samurai {
 
+	protected String domain;
+
+	public Retainer() {
+
+	}
+
+	public Retainer(String name, String domain) {
+		this.name = name;
+		this.domain = domain;
+	}
+
+	void getPaid() {
+		System.out.println("給料をもらうよ～。");
+	}
+
+	void work() {
+		System.out.println("年貢を取り立てるよ～。");
+	}
+
+	public String toString() {
+		return "拙者は" + domain + "藩士、" + name + "ともうす。";
+	}
+
+	public boolean equals(Object object) {
+		boolean isMatch;
+		Retainer retainer = (Retainer) object;
+		if (this.domain.equals(retainer.domain)) {
+			isMatch = true;
+		} else {
+			isMatch = false;
+		}
+		return isMatch;
+	}
+}
 
 //ここにRoninクラスを記述
+class Ronin extends Samurai {
 
+	public Ronin() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	void covered() {
+		System.out.println("傘張りするよ～。");
+	}
+
+	void work() {
+		System.out.println("傘張るよ～。");
+	}
+}
 
 public class CastleTown {
 
-    public static void main(String[] args) {
-        System.out.println("5人の侍を配列に詰めます。\n");
+	public static void main(String[] args) {
+		System.out.println("5人の侍を配列に詰めます。\n");
 
+		//ここに適切な処理を記述
+		Samurai[] samurais = new Samurai[5];
+		for (int i = 0; i < 5; i++) {
+			int s = (int) (Math.random() * 2);
+			if (s == 0) {
 
-        //ここに適切な処理を記述
+				samurais[i] = new Retainer();
+			} else {
+				samurais[i] = new Ronin();
+			}
+		}
+		System.out.println("詰め終わりました。\n");
+		System.out.println("それぞれの人数を表示してみます。\n");
 
+		int retainerCount = 0;
+		int roninCount = 0;
 
-        System.out.println("詰め終わりました。\n");
-        System.out.println("それぞれの人数を表示してみます。\n");
+		//ここに適切な処理を記述
+		for (int i = 0; i < 5; i++) {
 
+			if (samurais[i] instanceof Retainer) {
 
-        int retainerCount = 0;
-        int roninCount = 0;
+				retainerCount++;
+			} else {
 
+				roninCount++;
+			}
 
-        //ここに適切な処理を記述
+		}
 
-
-        System.out.println("藩士：" + retainerCount + "人");
-        System.out.println("\n浪人：" + roninCount + "人");
-    }
+		System.out.println("藩士：" + retainerCount + "人");
+		System.out.println("\n浪人：" + roninCount + "人");
+	}
 }
